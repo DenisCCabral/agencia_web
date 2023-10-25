56db3c3de7159147be7f1929e13d46028ee36452
DenisCCabral <denis.ccabral@gmail.com>
2023-10-25 19:51:27 -0300

    alterações de cor

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./reset.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,400;0,700;1,100;1,200;1,400;1,700&display=swap" rel="stylesheet">
+
+
 
     <title>Agência Web</title>
 </head>
@@ -36,8 +39,9 @@
             <strong>Construa sua</strong><br> <strong>Presença Oline</strong>
         </p>
         <p class="subtxt1">Deixe-nos construir <strong> sucesso digital.</strong>Trasforma-mos<br> sua visão em realidade online. Seu site, sua historia, seu <strong>sucesso</strong></p>
-        <img class="img1" src="./src/img/img1.jpg">
+        <img class="img1" src="./src/img/img1.png">
     </section>
+    <p></p>
 
 </body>
 
